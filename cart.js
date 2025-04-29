9d879832f7cc4309c489b72dbbcbfc081605f0e5
MDARIFULISLAM13 <mdarifulislamncs13@gmail.com>
2025-04-30 00:01:41 +0600

    module complete

---
+++ cart.js
@@ -0,0 +1,9 @@
+let print = () =>
+{
+
+    return "Print value";
+}
+let value = 10
+let number = 5;
+
+module.exports = { print, value, number };
---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+//module for js
+//cart.js
+
+const { print, number, value } = require( "./cart" );
+console.log( print() );
+console.log( number );
+console.log( value );
+
+//module done
