5f7e7ea9caabc00a4850762fe07af9291bbc1e78
MDARIFULISLAM13 <mdarifulislamncs13@gmail.com>
2025-07-10 04:10:39 +0600

    view data

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -7,25 +7,50 @@ let app = express();
 
 app.use( express.json() )
 
-app.get( "/student-read", ( req, res ) =>
-{
-    res.send( "Student view api\n" )
-} )
-app.post( "/student-insert", async ( req, res ) =>
+//view data;
+app.get( "/student-read", async ( req, res ) =>
 {
     let myDB = await dbconnection();
     let studentCollection = myDB.collection( "ALL_Students" )
-    let { sname, semail } = req.body;
-    let obj = { sname, semail };
-    let insertres = await studentCollection.insertOne( obj );
 
+    let data = await studentCollection.find().toArray();
     let resobj = {
         statusbar: 1,
-        msg: "Data insert\n",
-        insertres
+        msg: "Student List\n",
+        data
     }
     res.send( resobj );
+
 } )
+
+
+//data insert in bd
+app.post( "/student-insert", async ( req, res ) =>
+{
+    let myDB = await dbconnection();
+    let studentCollection = myDB.collection( "ALL_Students" );
+
+    let { sname, semail } = req.body;
+    let obj = { sname, semail };
+
+    // Check if email already exists
+    let existingStudent = await studentCollection.findOne( { semail } );
+    if ( existingStudent )
+    {
+        return res.send( "Email already found\n" );
+    }
+
+    // Insert if email not found
+    let insertres = await studentCollection.insertOne( obj );
+    let resobj = {
+        statusbar: 1,
+        msg: "Data inserted\n",
+        insertres
+    };
+
+    res.send( resobj );
+} );
+
 
 app.listen( "8000" );
 
